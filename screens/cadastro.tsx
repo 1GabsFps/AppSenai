65066832bd00194987a6c48857b9d005f5095bdc
1GabsFps <gabriel.neco@live.com>
2024-02-29 15:57:33 -0300

    Adiciona react-native-keyboard-aware-scroll-view ao cadastro

--- screens/cadastro.tsx
+++ screens/cadastro.tsx
@@ -1,30 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { View, ScrollView} from 'react-native';
 import React from 'react';
+import { View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import Logo from '../components/logo';
 import InputField from '../components/input';
 import Botao from '../components/botao';
 
-function cadastro () {
+export default function Cadastro() {
   return (
-    <View>
-
-        <View>
-            <Logo text="Cadastre-se" color="red" />
-        </View>
-        <ScrollView>
-            <InputField label="Nome" placeholder="Digite seu nome" hideText={false} />
-            <InputField label="Sobrenome" placeholder="Digite seu Sobrenome" hideText={false} />
-            <InputField label="Email" placeholder="Digite seu Email" hideText={false} />
-            <InputField label="Telefone" placeholder="Digite seu Telefone" hideText={false} />
-            <InputField label="Senha" placeholder="Digite sua Senha" hideText={true} />
-            <InputField label="Confirme sua Senha" placeholder="Confirme sua Senha" hideText={true}/>
-            <Botao text="Cadastrar" color="black" />
-
-        </ScrollView>
-        <StatusBar style="auto" />
-    </View>
+    <KeyboardAwareScrollView
+      resetScrollToCoords={{ x: 0, y: 0 }}
+      contentContainerStyle={{ flexGrow: 1 }}
+      scrollEnabled={false}
+    >
+      <View>
+        <Logo text="Cadastre-se" color="red" />
+        <InputField label="Nome" placeholder="Digite seu nome" hideText={false} />
+        <InputField label="Sobrenome" placeholder="Digite seu Sobrenome" hideText={false} />
+        <InputField label="Email" placeholder="Digite seu Email" hideText={false} />
+        <InputField label="Telefone" placeholder="Digite seu Telefone" hideText={false} />
+        <InputField label="Senha" placeholder="Digite sua Senha" hideText={true} />
+        <InputField label="Confirme sua Senha" placeholder="Confirme sua Senha" hideText={true}/>
+        <Botao text="Cadastrar" color="black" />
+      </View>
+    </KeyboardAwareScrollView>
   );
 }
-
-export default cadastro;
